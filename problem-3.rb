c01df481697b1026b3ae5ac87efecbac8fd6c216
Juan Alvarado <jalvarad@nearinfinity.com>
2013-03-04 10:47:54 -0500

    add new problem solutions

---
+++ problem-3.rb
@@ -0,0 +1,11 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+
+require 'prime'
+
+n = 600851475143
+
+factors, powers = n.prime_division.transpose
+
+puts factors
